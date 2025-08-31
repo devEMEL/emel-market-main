f63e51863facbb97f61021c41c3956012482e7fe
devEMEL <dominichenry882@gmail.com>
2025-08-31 18:32:56 +0100

    v1 done ..

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -42,12 +42,6 @@ const Header: React.FC = () => {
                             Auctions
                         </Link>
                         <Link
-                            href="/activity"
-                            className="bg-white hover:bg-gray-100 text-black px-4 py-2 rounded-lg font-medium flex items-center space-x-2 transition-colors cursor-pointer"
-                        >
-                            Activity
-                        </Link>
-                        <Link
                             href="/profile"
                             className="bg-white hover:bg-gray-100 text-black px-4 py-2 rounded-lg font-medium flex items-center space-x-2 transition-colors cursor-pointer"
                         >
@@ -118,13 +112,6 @@ const Header: React.FC = () => {
                             className="bg-white hover:bg-gray-100 text-black px-4 py-2 rounded-lg font-medium flex items-center space-x-2 transition-colors cursor-pointer"
                         >
                            Auctions
-                        </Link>
-                        <Link
-                            href="/activity"
-                            //   className="block text-white hover:text-white/80 px-3 py-2"
-                            className="bg-white hover:bg-gray-100 text-black px-4 py-2 rounded-lg font-medium flex items-center space-x-2 transition-colors cursor-pointer"
-                        >
-                            Activity
                         </Link>
                         <Link
                             href="/profile"
